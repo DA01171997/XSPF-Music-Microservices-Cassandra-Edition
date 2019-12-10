91c9b9995e035b90a46804ea9a139088f881fdd8
WellsonPan <dihydrogenmonoxide1337@gmail.com>
2019-12-09 21:55:07 -0800

    lazy overwrite/fix of this file

--- unit_test/playlistTest/playlistTest.sh
+++ unit_test/playlistTest/playlistTest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "This is test get all tracks"
+echo "1.This is test get all tracks"
 curl --verbose\
 	--request GET\
 	--header 'Content-Type: application/json'\
@@ -11,104 +11,104 @@ echo
 echo
 echo
 
-echo "This is get a playlist with an id. This is supposed to fail."
+echo "2.This is get a playlist with an id. This is supposed to fail."
 curl --verbose\
     --request GET\
-    http://127.0.0.1:8000/api/v1/collections/playlists/9999
+    http://127.0.0.1:8000/api/v1/collections/playlists/Playlist%2009
 #done
 
 echo
 echo
 echo
 
-echo "This is get a playlist with an id. This is supposed to succeed."
+echo "3.This is get a playlist with an id. This is supposed to succeed."
 curl --verbose\
     --request GET\
-    http://127.0.0.1:8000/api/v1/collections/tracks/1
+    http://127.0.0.1:8000/api/v1/collections/playlists/Playlist%2000
 #done
 
 echo
 echo
 echo
 
-echo "This is to delete a playlist with a certain id. This is supposed to fail."
+echo "4.This is to delete a playlist with a certain id. This is supposed to fail."
 curl --verbose \
     --request DELETE \
     --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists/9999
+    http://127.0.0.1:8000/api/v1/collections/playlists/Playlist%2009
 #done
 
 echo
 echo
 echo
 
-echo "This is to delete a playlist with a certain id. This is supposed to succeed."
+echo "5.This is to delete a playlist with a certain id. This is supposed to succeed."
 curl --verbose \
     --request DELETE \
     --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists/1
+    http://127.0.0.1:8000/api/v1/collections/playlists/Playlist%2000
 #done
 
 echo
 echo
 echo
 
-echo "This is to delete a playlist with a certain id. This is supposed to fail."
+echo "6.This is to delete a playlist with a certain id. This is supposed to fail."
 curl --verbose \
     --request DELETE \
     --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists/1
+    http://127.0.0.1:8000/api/v1/collections/playlists/Playlist%2000
 #done
 
+# echo
+# echo
+# echo
+
+# echo "7.This is to get a playlist with the provided arguments. This is supposed to fail."
+# curl --verbose \
+#     --request GET \
+#     --header 'Content-Type: application/json' \
+#     http://127.0.0.1:8000/api/v1/collections/playlists
+# #done
+
+# echo
+# echo
+# echo
+
+# echo "8.This is to get a playlist with the provided arguments. This is supposed to succeed."
+# curl --verbose \
+#     --request GET \
+#     --header 'Content-Type: application/json' \
+#     http://127.0.0.1:8000/api/v1/collections/playlists?playID=2
+# #done
+
+# echo
+# echo
+# echo
+
+# echo "9.This is to get a playlist with the provided arguments. This is supposed to succeed."
+# curl --verbose \
+#     --request GET \
+#     --header 'Content-Type: application/json' \
+#     http://127.0.0.1:8000/api/v1/collections/playlists?playTitle=Playlist%2002
+# #done
+
+# echo
+# echo
+# echo
+
+# echo "10.This is to get a playlist with the provided arguments. This is supposed to succeed."
+# curl --verbose \
+#     --request GET \
+#     --header 'Content-Type: application/json' \
+#     http://127.0.0.1:8000/api/v1/collections/playlists?userName=WassupMan404
+# #done
+
 echo
 echo
 echo
 
-echo "This is to get a playlist with the provided arguments. This is supposed to fail."
-curl --verbose \
-    --request GET \
-    --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists
-#done
-
-echo
-echo
-echo
-
-echo "This is to get a playlist with the provided arguments. This is supposed to succeed."
-curl --verbose \
-    --request GET \
-    --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists?playID=2
-#done
-
-echo
-echo
-echo
-
-echo "This is to get a playlist with the provided arguments. This is supposed to succeed."
-curl --verbose \
-    --request GET \
-    --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists?playTitle=Playlist%2002
-#done
-
-echo
-echo
-echo
-
-echo "This is to get a playlist with the provided arguments. This is supposed to succeed."
-curl --verbose \
-    --request GET \
-    --header 'Content-Type: application/json' \
-    http://127.0.0.1:8000/api/v1/collections/playlists?userName=WassupMan404
-#done
-
-echo
-echo
-echo
-
-echo "This is to post a playlist with the provided json. This is supposed to succeed."
+echo "7.This is to post a playlist with the provided json. This is supposed to succeed."
 curl --verbose \
     --request POST \
     --header 'Content-Type: application/json' \
@@ -120,7 +120,7 @@ echo
 echo
 echo
 
-echo "This is to post a playlist with the provided json. This is supposed to succeed w/o a playDesc."
+echo "8.This is to post a playlist with the provided json. This is supposed to succeed w/o a playDesc."
 curl --verbose \
     --request POST \
     --header 'Content-Type: application/json' \
@@ -132,7 +132,7 @@ echo
 echo
 echo
 
-echo "This is to post a playlist with the provided json. This is supposed to fail b/ malformed."
+echo "9.This is to post a playlist with the provided json. This is supposed to fail b/ malformed."
 curl --verbose \
     --request POST \
     --header 'Content-Type: application/json' \
