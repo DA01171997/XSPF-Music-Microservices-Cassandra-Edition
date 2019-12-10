6fc31985fb293d17f232266cc616614cb1448d15
duydo <duy.ado97@gmail.com>
2019-12-10 15:58:40 -0800

    Working xspfApi.py

--- xspfApi.py
+++ xspfApi.py
@@ -13,34 +13,25 @@ def getPlayListByID(id, playlists):
         if playlist['playID'] == id:
             return playlist
 
-def getUserByID(id):
-    userstr = "http://localhost:8000/api/v1/users/id/%d" % (id)
-    user = requests.get(userstr).json()
-    return user
-
-def getDescriptionByUser(uid):
-    descstr = "http://localhost:8000/api/v1/descriptions/users/%s/descriptions/all" % (uid)
-    desc = requests.get(descstr).json()
+def getDescriptionByUrl(playUserUserName, url):
+    url = "http://127.0.0.1:8000/api/v1/descriptions/users/{}/descriptions?trackMediaURL={}".format(playUserUserName,url)
+    response = requests.get(url)
+    if response.status_code == 200:
+        desc = response.json()
+        desc = desc[0]['descriptionDesc']
+    else:
+        desc = ""
     return desc
 
 def getPlayListURLs(playlist):
-    urls = playlist['playListOfTracks'].strip('[]').split(',')
-    playlist_urls = []
-    for url in urls:
-        playlist_urls.append(url.strip(' ').strip('\''))
-    return playlist_urls
+    tracklist = playlist['playListOfTracks']
+    return tracklist
 
 def getTrackInfoFromURL(urls, tracks, playlist):
     x = xspf.Xspf()
     x.title = playlist['playTitle']
-
-    user = getUserByID(playlist['playUserUserName'])
-
-    x.creator = user['userUserName']
-
-    desc = getDescriptionByUser(user['userUserName'])
+    x.creator = playlist['playUserUserName']
     x.annotation = playlist['playDesc']
-
     for url in urls:
         tr = xspf.Track()
         for track in tracks:
@@ -51,20 +42,25 @@ def getTrackInfoFromURL(urls, tracks, playlist):
                 tr.duration = str(track['trackLength']) # cast to string, cannot serialize otherwise.
                 tr.location = track['trackMediaURL']
                 tr.image = track['trackArt']
-                if track['trackMediaURL'] == desc[0]['trackMediaURL']:
-                    tr.annotation = desc[0]['descriptionDesc']
+                tr.annotation = getDescriptionByUrl(playlist['playUserUserName'],track['trackMediaURL'])
                 x.add_track(tr)
     return x
 
-@app.route("/api/v1/collections/playlists/<int:playID>.xspf", methods = ['GET'])
-def generate_xspf(playID):
-        playlistURL = "http://localhost:8000/api/v1/collections/playlists/" + str(playID)
-        playlist = requests.get(playlistURL).json()
+@app.route("/api/v1/collections/playlists/<string:playTitle>.xspf", methods = ['GET'])
+def generate_xspf(playTitle):
+    try:
+        playlistURL = "http://localhost:8000/api/v1/collections/playlists/" + playTitle
+        results = requests.get(playlistURL).json()
+        playlist = results[0]
         tracks = requests.get("http://localhost:8000/api/v1/collections/tracks/all").json()
         if playlist == {'message': 'This resource does not exist.'}:
             raise exceptions.NotFound
         if tracks == {'message': 'This resource does not exist.'}:
             raise exceptions.NotFound
         tracklist = getPlayListURLs(playlist)
+        print(tracklist[0])
+        print(playlist['playDesc'])
         xspf_playlist = getTrackInfoFromURL(tracklist, tracks, playlist)
         return xspf_playlist.toXml(), status.HTTP_200_OK
+    except Exception as e:
+        return { 'error': str(e) }, status.HTTP_404_NOT_FOUND
